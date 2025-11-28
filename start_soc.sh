5d01f7a3520f490f5380ce030ca3839959db521b
suleohis <suleephraim1@gmail.com>
2025-11-28 22:22:58 +0100

    DAY 28 SHIPPED – MkDocs + Material pro documentation site

--- start_soc.sh
+++ start_soc.sh
@@ -8,7 +8,7 @@ echo "Starting Cyber Blueprint 2026 Mini-SIEM..."
 echo "Generating fresh attack logs..."
 
 # Generate realistic attack + normal background
-python month1/generate_logs.py 25 --normal 15
+
 
 echo "Starting Flask dashboard on http://127.0.0.1:5000"
 echo "Press Ctrl+C to stop"
@@ -16,3 +16,6 @@ echo ""
 
 # Run Flask app
 python month1/app.py
+
+# Run StreamLit
+streamlit run month1/streamlit_dashboard.py 
